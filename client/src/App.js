606ddf0be0cc99ccdff94670fa9ebc55340c8c2b
slametsampon <slamet@pon.co.id>
2021-10-06 22:41:22 +0700

    working on layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,50 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-//components
-import CmmsNavigation from "./components/layout/CmmsNavigation";
+//componenets
+import ConfigNav from "./components/layout/ConfigNav";
+import EqpScope from "./components/Configuration/Equipment/EqpScope"
+import FromFile from "./components/Configuration/FromFile/FromFile";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
-        <CmmsNavigation />
-        <h4>It's CMMS Home</h4>
+        <Router>
+            <Navbar expand="lg" bg="light">
+                <Navbar.Brand href="/">CMMS-OXO</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#workOrder">Work Order</Nav.Link>
+                    <Nav.Link href="#PM & PdM">PM & PdM</Nav.Link>
+                    <Nav.Link href="#reporting">Reporting</Nav.Link>
+                    <ConfigNav />
+                    <Nav.Link href="#help">Help</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+                <Route exact path="/">
+                    It's Home CMMS - OXO
+                </Route>
+                <Route exact path="/eqpScope">
+                    <EqpScope />
+                </Route>
+                <Route exact path="/file">
+                    <FromFile />
+                </Route>
+            </Switch>
+        </Router>
       </div>
     </Fragment>
   );
---
+++ client/src/components/Configuration/FromFile/FromFile.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function FromFile() {
+  return (
+    <Fragment>
+        <h4>It's From File Configuration</h4>
+    </Fragment>
+  );
+}
+
+export default FromFile;
